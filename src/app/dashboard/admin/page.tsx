3bfee095d6b5007eeb5db76ee47c33c02e270e9a
Claudio Oliveira <dev.caon@gmail.com>
2024-09-20 21:06:10 -0300

    adicionado o componente CountChart

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,10 +1,12 @@
+import AttendanceChart from "@/components/AttendanceChart";
+import CountChart from "@/components/CountChart";
 import UserCard from "@/components/UserCard";
 import React from "react";
 
 const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
-      <div className="w-full lg:w-2/3">
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
         {/* user cards */}
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="student" />
@@ -12,6 +14,20 @@ const AdminPage = () => {
           <UserCard type="parent" />
           <UserCard type="staff" />
         </div>
+
+        {/* middle chart */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* count chart */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* attendance chat */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart />
+          </div>
+        </div>
+        {/* bottom chart */}
+        <div className=""></div>
       </div>
       <div className="w-full lg:w-1/3">Left side</div>
     </div>
